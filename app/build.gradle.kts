87292be52747469c1a9bc2b1bb4f822f09253e94
Pavels Urbanovics <pavlik@upanet.org>
2026-07-29 00:46:56 +0300

    bugfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.smarthome.lv"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         val envFile = rootProject.file(".env")
         if (envFile.exists()) {
