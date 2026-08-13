639e4bdb2a44d726ad24e70a5501899eaa2d6cdb
Pavels Urbanovics <pavlik@upanet.org>
2026-08-14 00:38:15 +0300

    Redesign sensors view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.smarthome.lv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.0.5"
+        versionCode = 10
+        versionName = "0.0.6"
 
         val envFile = rootProject.file(".env")
         if (envFile.exists()) {
